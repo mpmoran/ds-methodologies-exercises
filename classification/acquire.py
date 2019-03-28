6d8473400ffb4d64d0fdd5086e0dd1a5b5570066
Michael P. Moran <mmutexas@gmail.com>
2019-03-28 09:58:02 -0500

    add functions to acquire titanic and iris data from mysql db

---
+++ classification/acquire.py
@@ -0,0 +1,39 @@
+# Getting data from SQL databases
+from env import host, user, password
+import pandas as pd
+from sqlalchemy import create_engine
+
+def get_db_url(
+    hostname: str, username: str, password: str, db_name: str
+) -> str:
+    """
+    return url for accessing a mysql database
+    """
+    return f"mysql+pymysql://{username}:{password}@{hostname}/{db_name}"
+
+
+def get_sql_conn(hostname: str, username: str, password: str, db_name: str):
+    """
+    return a mysql connection object
+    """
+    return create_engine(get_db_url(host, user, password, db_name))
+
+
+def df_from_sql(conn, query: str) -> pd.DataFrame:
+    """
+    return a Pandas DataFrame resulting from a sql query
+    """
+    return pd.read_sql(query, conn)
+
+def get_titanic_data() -> pd.DataFrame:
+    tdb = "titanic_db"
+    query = ("SELECT * "
+             "FROM passengers;")
+    return df_from_sql(get_sql_conn(host, user, password, tdb), query)
+
+def get_iris_data() -> pd.DataFrame:
+    idb = "iris_db"
+    query = ("SELECT * "
+             "FROM measurements "
+             "JOIN species USING(species_id);")
+    return df_from_sql(get_sql_conn(host, user, password, idb), query)
