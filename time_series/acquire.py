177071bc774eac68af437ae157afc3661e6afc72
Michael P. Moran <mmutexas@gmail.com>
2019-04-17 22:27:03 -0500

    now chooses csv file or downloads and saves it if missing

--- time_series/acquire.py
+++ time_series/acquire.py
@@ -1,49 +1,53 @@
+import os
 import requests
 import pandas as pd
 
 
 def get_from_zach_api(table) -> pd.DataFrame:
-    base_url = "https://python.zach.lol"
-    endpoint = f"/api/v1/{table}"
+    root_endpoint = "https://python.zach.lol"
+    path = f"/api/v1/{table}"
 
-    data = requests.get(base_url + endpoint).json()
+    data = requests.get(root_endpoint + path).json()
     max_page = data["payload"]["max_page"]
     entries = []
     for i in range(1, max_page + 1):
-        url = base_url + endpoint + f"?page={i}"
+        url = root_endpoint + path + f"?page={i}"
         data = requests.get(url).json()
         entries += data["payload"][table]
 
     return pd.DataFrame(entries)
 
 
+def get_table(table_name):
+    csv_fname = f"{table_name}.csv"
+    if os.path.exists(csv_fname):
+        print(f"Reading {csv_fname} . . . .")
+        return pd.read_csv(csv_fname)
+    else:
+        df_table = get_from_zach_api(table_name)
+        df_table.to_csv(csv_fname, index=False)
+        return df_table
+
+
 def get_items() -> pd.DataFrame:
-    return get_from_zach_api("items")
+    return get_table("items")
 
 
 def get_stores() -> pd.DataFrame:
-    return get_from_zach_api("stores")
+    return get_table("stores")
 
 
 def get_sales() -> pd.DataFrame:
-    return get_from_zach_api("sales")
-
-
-def download_product_info() -> None:
-    get_items().to_csv("items.csv")
-    get_stores().to_csv("stores.csv")
-    get_sales().to_csv("sales.csv")
-
-
-def read_product_info() -> tuple:
-    items = pd.read_csv("items.csv", index_col=0)
-    stores = pd.read_csv("stores.csv", index_col=0)
-    sales = pd.read_csv("sales.csv", index_col=0)
-    return items, stores, sales
+    return get_table("sales")
 
 
 def get_product_info() -> pd.DataFrame:
-    items, stores, sales = read_product_info()
-    return sales.merge(
+    items = get_items()
+    stores = get_stores()
+    sales = get_sales()
+
+    merged = sales.merge(
         items, how="left", left_on="item", right_on="item_id"
     ).merge(stores, how="left", left_on="store", right_on="store_id")
+
+    return merged.drop(columns=["item", "store"])
